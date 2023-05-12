e19f11190bf031967eca886f543fbbac37b47b8c
John Doe <johndoe@email.com>
2023-05-12 19:39:50 +0430

    change

--- src/visitor/nameAnalyzer/NameAnalyzer.java
+++ src/visitor/nameAnalyzer/NameAnalyzer.java
@@ -35,43 +35,43 @@ public class NameAnalyzer extends Visitor<Void> {
         return null;
     }
 
-    
     @Override
     public Void visit(FuncDeclaration funcDeclaration) {
         var functionItem = new FunctionItem(funcDeclaration);
         var functionSymbolTable = new SymbolTable(SymbolTable.top, funcDeclaration.getName().getName());
         functionItem.setFunctionSymbolTable(functionSymbolTable);
-        functio
+        functionItem.setFunctionDeclaration(funcDeclaration);
+        functionItem.setName(funcDeclaration.getName().getName());
 
         funcDeclaration.setArgs(funcDeclaration.getArgs());
         funcDeclaration.setStatements(funcDeclaration.getStatements());
         funcDeclaration.setIdentifier(funcDeclaration.getIdentifier());
 
         try {
-            Sy mbolTable.root.put(functionItem);
-        }catch (ItemAlreadyExistsException e)
-        {
-        FunctionRedefinition functionRedefinition = new
-        FunctionRedefinition(funcDeclaration.getLine(),funcDeclaration.getName().getName());
+            SymbolTable.root.put(functionItem);
+        } catch (ItemAlreadyExistsException e) {
+            FunctionRedefinition functionRedefinition = new FunctionRedefinition(funcDeclaration.getLine(),
+                    funcDeclaration.getName().getName());
 
-        this.nameErrors.add(functionRedefinition);
+            this.nameErrors.add(functionRedefinition);
         }
         SymbolTable symbolTable = new SymbolTable();
         symbolTable.pre = SymbolTable.top;
         functionItem.setFunctionSymbolTable(symbolTable);
         SymbolTable.push(symbolTable);
 
-        if(funcDeclaration.getName() != null)
+        if (funcDeclaration.getName() != null)
             funcDeclaration.getName().accept(this);
 
-        if(funcDeclaration.getIdentifier() != null)
- 
+        if (funcDeclaration.getIdentifier() != null)
+            funcDeclaration.getIdentifier().accept(this);
+
         for (ArgDeclaration varDeclaration : funcDeclaration.getArgs()) {
             varDeclaration.accept(this);
         }
 
         for (var stmt : funcDeclaration.getStatements()) {
-            if(stmt instanceof VarDecStmt) {
+            if (stmt instanceof VarDecStmt) {
                 stmt.accept(this);
             }
         }
@@ -88,17 +88,17 @@ public class NameAnalyzer extends Visitor<Void> {
         variableItem.setVarDeclaration(varDeclaration);
 
         try {
-        SymbolTable.root.put(variableItem);
-        }catch (ItemAlreadyExistsException e){
-        VariableRedefinition variableRedefinition = new
-        VariableRedefinition(varDeclaration.getLine(),varDeclaration.getIdentifier().getName());
-        this.nameErrors.add(variableRedefinition);
+            SymbolTable.root.put(variableItem);
+        } catch (ItemAlreadyExistsException e) {
+            VariableRedefinition variableRedefinition = new VariableRedefinition(varDeclaration.getLine(),
+                    varDeclaration.getIdentifier().getName());
+            this.nameErrors.add(variableRedefinition);
 
         }
-        if(varDeclaration.getIdentifier()!= null)
+        if (varDeclaration.getIdentifier() != null)
             varDeclaration.getIdentifier().accept(this);
 
-        if(varDeclaration.getInitialExpression()!=null)
+        if (varDeclaration.getInitialExpression() != null)
             varDeclaration.getInitialExpression().accept(this);
 
         return null;
